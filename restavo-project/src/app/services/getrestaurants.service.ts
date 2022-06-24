9e010e498a06d93a8d6e5b571f5185be23f750fb
gulduomer <43722346+omerfguldu@users.noreply.github.com>
2022-06-24 14:30:15 +0300

    last commit

--- restavo-project/src/app/services/getrestaurants.service.ts
+++ restavo-project/src/app/services/getrestaurants.service.ts
@@ -6,7 +6,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class GetrestaurantsService {
   restId;
+
   constructor(private http: HttpClient) {}
+
   getRestaurants() {
     return this.http.get(
       `http://172.16.88.73:8078/getRestaurants/${this.restId}`
